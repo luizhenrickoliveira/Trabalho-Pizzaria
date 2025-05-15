0721db7aac264a4dda98ccffe6a199bbe2d2f26a
luizhenrickoliveira <luiz.oliveira.matos@escola.pr.gov.br>
2025-05-15 11:34:55 -0300

    Delete pizzaria directory
    refazendo

--- pizzaria/function.js
+++
@@ -1,9 +0,0 @@
-function logar ()
-   var login=document.gpteleventbyID ('Login').value;
-   var senha=document.gpteleventbyID ('Senha').value;
-   var email=document.gpteleventbyID ('Email').value;
-
-   if (login=="admin" && senha=="123") { alert ('sucesso');
-   location.href="home.html";}
-
-   else{ alert ('Usuário ou senha incorretas');}
